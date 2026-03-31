b8e569abcd681a54020dc098ea906e5ed1be37cb
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2026-03-31 19:39:51 +0300

    ThreadsPool

---
+++ Lesson4-Threads2/Threads/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+
+namespace Threads
+{
+    internal class Program
+    {
+        static void SampleTask(object state)
+        {
+            string data = (string)state;
+            Console.WriteLine(data);
+        }
+
+        static void Main(string[] args)
+        {
+
+            int workerThreads = 0;
+            int completionThreads = 0;
+            
+            ThreadPool.SetMinThreads(100, 100);
+            ThreadPool.SetMaxThreads(100, 100);
+            ThreadPool.GetAvailableThreads(out workerThreads, out completionThreads);
+            Console.WriteLine($"Worker threads: {workerThreads}\n Completion threads: {completionThreads}");
+            Task.Run(() =>
+            {
+                Console.WriteLine("Task from pool");
+            });
+            ThreadPool.QueueUserWorkItem((state) =>
+            {
+                Console.WriteLine($"Task: {state}");
+            });
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback(SampleTask), "State");
+
+            
+            //Console.WriteLine($"Worker threads: {workerThreads}\n Completion threads: {completionThreads}");
+
+           
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
