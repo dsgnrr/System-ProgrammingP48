05b8767d935120d5176aaad52d4774c6a0f40bcb
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2026-04-02 18:02:49 +0300

    Update Program.cs

--- Lesson4-Threads2/Threads/Program.cs
+++ Lesson4-Threads2/Threads/Program.cs
@@ -1,16 +1,54 @@
 ﻿using System.Threading;
-
+using System.Net.Http;
 namespace Threads
 {
     internal class Program
     {
+        static async Task<string> GetDataAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(3000);
+            cancellationToken.ThrowIfCancellationRequested();
+            using HttpClient client = new HttpClient();
+            string result = await client.GetStringAsync(@"https://randomuser.me/api/");
+            return $"GET data: {result}";
+        }
+
+        static async Task Main(string[] args)
+        {
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            //string result = await GetDataAsync();
+            //Console.WriteLine(result);
+            var task = GetDataAsync(token);
+
+            await Task.Delay(1000);
+            cts.Cancel();
+
+            //while (!task.IsCompleted)
+            //{
+            //    Console.Write(".");
+            //    await Task.Delay(1000);
+            //}
+
+            try
+            {
+                Console.WriteLine(await task);
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Task was canceled");
+            }
+        }
+
+        #region ThreadsPool
         static void SampleTask(object state)
         {
             string data = (string)state;
             Console.WriteLine(data);
         }
 
-        static void Main(string[] args)
+        static void threads_pool()
         {
 
             int workerThreads = 0;
@@ -39,4 +77,5 @@ namespace Threads
             Console.WriteLine("Hello, World!");
         }
     }
+    #endregion
 }
