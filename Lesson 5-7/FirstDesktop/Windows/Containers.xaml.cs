c71ab76f4ae7e363de68804c2a1117e98b205612
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2026-04-21 18:47:51 +0300

    2

--- Lesson 5-7/FirstDesktop/Windows/Containers.xaml.cs
+++ Lesson 5-7/FirstDesktop/Windows/Containers.xaml.cs
@@ -20,6 +20,38 @@ namespace FirstDesktop.Windows
         public Containers()
         {
             InitializeComponent();
+            pushWrap();
+        }
+
+        private void pushWrap()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                wrapPanel.Children.Add(generateButton((i+1).ToString()));
+            }
+        }
+        private Button generateButton(string id)
+        {
+            Random rnd = new Random();
+            Button b = new Button();
+            b.Height = 70;
+            b.Width = 70;
+            b.Margin = new Thickness(10);
+            b.Background = new SolidColorBrush(
+                Color.FromRgb(
+                    (byte)rnd.Next(0, 255),
+                    (byte)rnd.Next(0, 255),
+                    (byte)rnd.Next(0, 255)
+                    ));
+            b.FontWeight = FontWeights.Bold;
+            b.FontSize = 25;
+            b.Content = $"#{id}";
+            return b;
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Ok");
         }
     }
 }
